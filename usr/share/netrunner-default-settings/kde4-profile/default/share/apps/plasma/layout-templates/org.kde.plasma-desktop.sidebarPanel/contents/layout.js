43f6e65c812beb9fb0a400bf7e5e2d1545c252d4
Leszek Lesner <leszek.lsner@web.de>
2013-11-29 12:36:59 +0100

    Added new plasma panel layout (sidebarPanel)

---
+++ usr/share/netrunner-default-settings/kde4-profile/default/share/apps/plasma/layout-templates/org.kde.plasma-desktop.sidebarPanel/contents/layout.js
@@ -0,0 +1,46 @@
+var panel = new Panel;
+panel.location = 'right';
+panel.alignment = 'left';
+panel.hiding = 'windowsbelow';
+panel.height = 250;
+panel.length = screenGeometry(panel.screen).height;
+
+
+var player = panel.addWidget("mediaplayer");
+
+
+var mixer = panel.addWidget("veromix-plasmoid");
+
+
+panel.addWidget("panelspacer_internal");
+
+
+var toggle = panel.addWidget("paneltoggleapplet")
+toggle.globalShortcut = "F11"
+toggle.writeConfig("Mode0", "4");
+toggle.writeConfig("Mode1", "2");
+toggle.writeConfig("FirstStart", "false");
+
+
+var clock = panel.addWidget("digital-clock");
+clock.writeConfig("Share","false")
+
+clock.writeConfig("announceInterval","0")
+clock.writeConfig("calendarType","locale")
+clock.writeConfig("dateStyle","0")
+clock.writeConfig("defaultTimezone","Local")
+clock.writeConfig("displayEvents","true")
+clock.writeConfig("displayHolidays","false")
+clock.writeConfig("holidaysRegion","")
+clock.writeConfig("holidaysRegions","")
+clock.writeConfig("holidaysRegionsDaysOff","")
+clock.writeConfig("plainClockColor","255,255,255")
+clock.writeConfig("plainClockDrawShadow","false")
+clock.writeConfig("plainClockFont","Roboto,12,-1,5,50,0,0,0,0,0")
+clock.writeConfig("showDay","false")
+clock.writeConfig("showSeconds","false")
+clock.writeConfig("showTimezone","false")
+clock.writeConfig("timeZones","")
+clock.writeConfig("useCustomColor","true")
+clock.writeConfig("useCustomShadowColor","false")
+
