8258ca4c52b8d33e7019b941d71cd8d1c4bba13c
Leszek Lesner <leszek.lsner@web.de>
2013-12-03 11:35:03 +0100

    New javascript 00-defaultLayout and panel layout.js Removed plasma-desktop-appletsrc as we use javascript now

--- usr/share/netrunner-default-settings/kde4-profile/default/share/apps/plasma-desktop/init/00-defaultLayout.js
+++ usr/share/netrunner-default-settings/kde4-profile/default/share/apps/plasma-desktop/init/00-defaultLayout.js
@@ -1,52 +1,51 @@
-loadTemplate("org.kde.plasma-desktop.defaultPanel")
-loadTemplate("org.kde.plasma-desktop.sidebarPanel")
+var actionPlugins = ConfigFile("plasma-desktop-appletsrc");
+actionPlugins.group = "ActionPlugins";
+
+var rightButton = ConfigFile(actionPlugins);
+rightButton.group = "RightButton;NoModifier";
+rightButton.writeEntry("_add panel", "true");
+rightButton.writeEntry("_context", "true");
+rightButton.writeEntry("_lock_screen", "false");
+rightButton.writeEntry("_logout", "false");
+rightButton.writeEntry("_run_command", "false");
+rightButton.writeEntry("_sep1", "false");
+rightButton.writeEntry("_sep2", "true");
+rightButton.writeEntry("_sep3", "false");
+rightButton.writeEntry("_wallpaper", "true");
+rightButton.writeEntry("add sibling containment", "false");
+rightButton.writeEntry("add widgets", "true");
+rightButton.writeEntry("configure", "true");
+rightButton.writeEntry("configure shortcuts", "false");
+rightButton.writeEntry("lock widgets", "true");
+rightButton.writeEntry("manage activities", "false");
+rightButton.writeEntry("remove", "true");
+
+delete rightButton;
+delete actionPlugins;
+
+loadTemplate("org.kde.plasma-desktop.defaultPanel");
+loadTemplate("org.kde.plasma-desktop.sidebarPanel");
 
 for (var i = 0; i < screenCount; ++i) {
-    var desktop = new Activity("netrunnerdesktop")
-    desktop.name = i18n("Desktop")
-    desktop.screen = i
-    desktop.writeConfig("alignToGrid", "true")
-    desktop.writeConfig("blankLabel", "false")
-    desktop.writeConfig("clickForFolderPreviews", "false")
-    desktop.writeConfig("customIconSize", "64")
-    desktop.writeConfig("numTextLines", "2")
-    desktop.writeConfig("previewPlugins", "imagethumbnail,jpegthumbnail,windowsexethumbnail,kffmpegthumbnailer,windowsimagethubmnail")
-    desktop.writeConfig("savedPositions", "1,6,ubiquity-kdeui.desktop,10,136,welcome.desktop,618,10,webaccounts.desktop,314,10,steam.desktop,466,10,mycomputer.desktop,10,10,Network,162,10")
-    desktop.writeConfig("sortDirsFirst", "false")
-    
+    var desktop = new Activity("netrunnerdesktop");
+    desktop.name = i18n("Desktop");
+    desktop.screen = i;
+
+    desktop.writeConfig("alignToGrid", "true");
+    desktop.writeConfig("blankLabel", "false");
+    desktop.writeConfig("clickForFolderPreviews", "false");
+    desktop.writeConfig("customIconSize", "64");
+    desktop.writeConfig("numTextLines", "2");
+    desktop.writeConfig("previewPlugins", "imagethumbnail,jpegthumbnail,windowsexethumbnail,kffmpegthumbnailer,windowsimagethubmnail");
+    desktop.writeConfig("savedPositions", "1,6,ubiquity-kdeui.desktop,10,136,welcome.desktop,618,10,webaccounts.desktop,314,10,steam.desktop,466,10,mycomputer.desktop,10,10,Network,162,10");
+    desktop.writeConfig("sortDirsFirst", "false");
+
     desktop.wallpaperPlugin = 'image'
     desktop.wallpaperMode = 'SingleImage'
-    
-    desktop.currentConfigGroup = new Array("Wallpaper", "image")
-    desktop.writeConfig("wallpaper", "/usr/share/wallpapers/Enigma II")
-    desktop.writeConfig("slidepaths", "/usr/share/wallpapers")
-    desktop.writeConfig("wallpaperposition", "2")
-    
-    desktop.currentConfigGroup = Array("ActionPlugins","RightButton;NoModifier")
-    desktop.writeConfig("_add panel","true")
-    desktop.writeConfig("_context", "true")
-    desktop.writeConfig("_lock_screen", "false")
-    desktop.writeConfig("_logout", "false")
-    desktop.writeConfig("_run_command", "false")
-    desktop.writeConfig("_sep1", "false")
-    desktop.writeConfig("_sep2", "true")
-    desktop.writeConfig("_sep3", "false")
-    desktop.writeConfig("_wallpaper", "true")
-    desktop.writeConfig("add sibling containment", "false")
-    desktop.writeConfig("add widgets", "true")
-    desktop.writeConfig("configure", "true")
-    desktop.writeConfig("configure shortcuts", "false")
-    desktop.writeConfig("lock widgets", "true")
-    desktop.writeConfig("manage activities", "false")
-    desktop.writeConfig("remove", "true")
 
-    //Create more panels for other screens // DON'T
-//     if (i > 0){
-//         var panel = new Panel
-//         panel.screen = i
-//         panel.location = 'bottom'
-//         panel.height = screenGeometry(i).height > 1024 ? 33 : 33
-//         var tasks = panel.addWidget("tasks")
-//         tasks.writeConfig("showOnlyCurrentScreen", true);
-//     }
+    desktop.currentConfigGroup = new Array("Wallpaper", "image");
+    desktop.writeConfig("wallpaper", "/usr/share/wallpapers/Enigma II");
+    desktop.writeConfig("slidepaths", "/usr/share/wallpapers");
+    desktop.writeConfig("slideTimer", "10");
+    desktop.writeConfig("wallpapercolor", "56,111,150");
 }
--- usr/share/netrunner/adjustments/kde/layout.js
+++ usr/share/netrunner/adjustments/kde/layout.js
@@ -1,106 +1,85 @@
-var panel = new Panel
+var panel = new Panel;
+
 if (panelIds.length == 1) {
     // we are the only panel, so set the location for the user
-    panel.location = 'bottom'
+    panel.location = "bottom";
 }
 
-panel.height = 33;
+panel.height = 38;
 
-//launcher = panel.addWidget("simplelauncher");
-//launcher.writeConfig("format", "NameDescription");
-//launcher.globalShortcut = "Alt+F1"
-//launcher.writeConfig("icon", "/usr/share/netrunner/netrunner-kmenu.png");
-//launcher.writeConfig("maxRecentApps", "5")
-//launcher.writeConfig("views", "RecentlyUsedApplications,Applications,RunCommand,Leave");
-
-launcher = panel.addWidget("org.kde.homerun-kicker")
-launcher.currentConfigGroup = Array("General")
-
-// This does currently not work for QML Widgets
+launcher = panel.addWidget("org.kde.homerun-kicker");
+launcher.globalShortcut = "Alt+F1";
+launcher.currentConfigGroup = Array("General");
 launcher.writeConfig("buttonImage", "file:///usr/share/netrunner/netrunner-kmenu.png");
 launcher.writeConfig("useCustomButtonImage", "true");
 
+spacer = panel.addWidget("panelspacer_internal");
+spacer.geometry = new QRectF(0, 0, 9, panel.height);
+spacer.writeConfig("FixedSize", "true");
 
-//var dolphin = panel.addWidget("icon");
-//dolphin.writeConfig("Url", "file:///usr/share/applications/kde4/dolphin.desktop"); 
-//dolphin.writeConfig("Order", "2"); 
-
-//var firefox = panel.addWidget("icon");
-//firefox.writeConfig("Url", "file:///usr/share/applications/kde4/firefox.desktop"); 
-//firefox.writeConfig("Order", "3"); 
-
-
-tasks = panel.addWidget("expanding-icons-taskmanager")
-tasks.writeConfig("Share","false")
-tasks.writeConfig("forceRows","false")
-tasks.writeConfig("groupWhenFull","true")
-tasks.writeConfig("groupingStrategy","0")
-tasks.writeConfig("highlightWindows","false")
-tasks.writeConfig("maxRows","1")
-tasks.writeConfig("showOnlyCurrentActivity","true")
-tasks.writeConfig("showOnlyCurrentDesktop","true")
-tasks.writeConfig("showOnlyCurrentScreen","false") 
-tasks.writeConfig("showOnlyMinimized","false")
-tasks.writeConfig("showTooltip","true")
-
-tasks.currentConfigGroup = Array("Launchers")
-tasks.writeConfig("Items","file:///usr/share/applications/kde4/dolphin.desktop?wmClass=Dolphin,file:///usr/share/applications/firefox.desktop?wmClass=Firefox")
-
+tasks = panel.addWidget("expanding-icons-taskmanager");
+tasks.writeConfig("Share","false");
+tasks.writeConfig("forceRows","false");
+tasks.writeConfig("groupWhenFull","true");
+tasks.writeConfig("groupingStrategy","0");
+tasks.writeConfig("highlightWindows","false");
+tasks.writeConfig("maxRows","1");
+tasks.writeConfig("showOnlyCurrentActivity","true");
+tasks.writeConfig("showOnlyCurrentDesktop","true");
+tasks.writeConfig("showOnlyCurrentScreen","false");
+tasks.writeConfig("showOnlyMinimized","false");
+tasks.writeConfig("showTooltip","true");
+tasks.currentConfigGroup = Array("Launchers");
+tasks.writeConfig("Items","file:///usr/share/applications/kde4/dolphin.desktop?wmClass=Dolphin,file:///usr/share/applications/firefox.desktop?wmClass=Firefox");
 
 var yakuake = panel.addWidget("icon");
-yakuake.writeConfig("Url", "file:///usr/share/applications/kde4/yakuake.desktop"); 
-yakuake.writeConfig("Order", "5"); 
+yakuake.writeConfig("Url", "file:///usr/share/applications/kde4/yakuake.desktop");
 
+spacer = panel.addWidget("panelspacer_internal");
+spacer.geometry = new QRectF(0, 0, 9, panel.height);
+spacer.writeConfig("FixedSize", "true");
 
 var systemtray = panel.addWidget("systemtray");
+systemtray.writeConfig("DefaultAppletsAdded", "true");
+systemtray.writeConfig("ShowApplicationStatus","true");
+systemtray.writeConfig("ShowCommunications","true");
+systemtray.writeConfig("ShowHardware","true");
+systemtray.writeConfig("ShowSystemServices","true");
+systemtray.writeConfig("ShowUnknown","true");
+systemtray.writeConfig("alwaysShown","Konversation,org.kde.ktp-presence");
+systemtray.writeConfig("hidden","KDE Wallet Manager,battery,easystroke");
+systemtray.currentConfigGroup = Array("Applets","1");
+systemtray.writeConfig("plugin","notifier");
+systemtray.currentConfigGroup = Array("Applets","2");
+systemtray.writeConfig("plugin","org.kde.ktp-presence");
+systemtray.currentConfigGroup = Array("Applets","3");
+systemtray.writeConfig("plugin","battery");
+systemtray.currentConfigGroup = Array("Applets","4");
+systemtray.writeConfig("plugin","org.kde.networkmanagement");
 
-systemtray.currentConfigGroup = Array("Applets","998")
-systemtray.writeConfig("plugin","org.kde.ktp-presence")
-systemtray.currentConfigGroup = Array()
- 
-systemtray.currentConfigGroup = Array("Applets","997")
-systemtray.writeConfig("plugin","battery")
-systemtray.currentConfigGroup = Array()
-
-systemtray.currentConfigGroup = Array("Applets","996")
-systemtray.writeConfig("plugin","org.kde.plasma-nm")
-systemtray.currentConfigGroup = Array("Applets","995")
-systemtray.writeConfig("plugin","org.kde.networkmanagement")
-systemtray.currentConfigGroup = Array()
-
-systemtray.writeConfig("ShowApplicationStatus","true")
-systemtray.writeConfig("ShowCommunications","true")
-systemtray.writeConfig("ShowHardware","true")
-systemtray.writeConfig("ShowSystemServices","true")
-systemtray.writeConfig("ShowUnknown","true")
-systemtray.writeConfig("alwaysShown","Konversation,org.kde.ktp-presence")
-systemtray.writeConfig("hidden","KDE Wallet Manager,org.kde.notifications,battery,easystroke")
- 
 var clock = panel.addWidget("digital-clock");
-clock.writeConfig("Share","false")
-
-clock.writeConfig("announceInterval","0")
-clock.writeConfig("calendarType","locale")
-clock.writeConfig("dateStyle","0")
-clock.writeConfig("defaultTimezone","Local")
-clock.writeConfig("displayEvents","true")
-clock.writeConfig("displayHolidays","false")
-clock.writeConfig("holidaysRegion","")
-clock.writeConfig("holidaysRegions","")
-clock.writeConfig("holidaysRegionsDaysOff","")
-clock.writeConfig("plainClockColor","255,255,255")
-clock.writeConfig("plainClockDrawShadow","false")
-clock.writeConfig("plainClockFont","Roboto,12,-1,5,50,0,0,0,0,0")
-clock.writeConfig("showDay","false")
-clock.writeConfig("showSeconds","false")
-clock.writeConfig("showTimezone","false")
-clock.writeConfig("timeZones","")
-clock.writeConfig("useCustomColor","true")
-clock.writeConfig("useCustomShadowColor","false")
+clock.writeConfig("Share","false");
+clock.writeConfig("announceInterval","0");
+clock.writeConfig("calendarType","locale");
+clock.writeConfig("dateStyle","0");
+clock.writeConfig("defaultTimezone","Local");
+clock.writeConfig("displayEvents","true");
+clock.writeConfig("displayHolidays","false");
+clock.writeConfig("holidaysRegion","");
+clock.writeConfig("holidaysRegions","");
+clock.writeConfig("holidaysRegionsDaysOff","");
+clock.writeConfig("plainClockColor","255,255,255");
+clock.writeConfig("plainClockDrawShadow","false");
+clock.writeConfig("plainClockFont","Roboto,12,-1,5,50,0,0,0,0,0");
+clock.writeConfig("showDay","false");
+clock.writeConfig("showSeconds","false");
+clock.writeConfig("showTimezone","false");
+clock.writeConfig("timeZones","");
+clock.writeConfig("useCustomColor","true");
+clock.writeConfig("useCustomShadowColor","false");
 
 var krunner = panel.addWidget("icon");
-krunner.writeConfig("Url", "file:///usr/share/applications/kde4/krunner.desktop"); 
-krunner.writeConfig("Order", "8"); 
+krunner.writeConfig("Url", "file:///usr/share/applications/kde4/krunner.desktop");
 
 var notifications = panel.addWidget("org.kde.notifications");
-notifications.writeConfig("Order", "9");
+
