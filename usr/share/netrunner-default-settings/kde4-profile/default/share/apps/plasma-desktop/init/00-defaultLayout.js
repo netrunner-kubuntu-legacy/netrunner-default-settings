277d7978c5742834e3444b93d5a717269a953080
Leszek Lesner <leszek.lsner@web.de>
2013-12-06 17:57:56 +0100

    Updated 00-defaultLayout.js for new default sorting method on desktop

--- usr/share/netrunner-default-settings/kde4-profile/default/share/apps/plasma-desktop/init/00-defaultLayout.js
+++ usr/share/netrunner-default-settings/kde4-profile/default/share/apps/plasma-desktop/init/00-defaultLayout.js
@@ -39,8 +39,9 @@ for (var i = 0; i < screenCount; ++i) {
     desktop.writeConfig("clickForFolderPreviews", "false");
     desktop.writeConfig("customIconSize", "64");
     desktop.writeConfig("numTextLines", "2");
+    desktop.writeConfig("sortColumn", "-1");
     desktop.writeConfig("previewPlugins", "imagethumbnail,jpegthumbnail,windowsexethumbnail,kffmpegthumbnailer,windowsimagethubmnail");
-    desktop.writeConfig("savedPositions", "1,6,ubiquity-kdeui.desktop,10,136,welcome.desktop,618,10,webaccounts.desktop,314,10,steam.desktop,466,10,mycomputer.desktop,10,10,Network,162,10");
+    desktop.writeConfig("savedPositions", "1,5,mycomputer.desktop,10,10,Network,162,10,welcome.desktop,314,10,webaccounts.desktop,466,10,steam.desktop,618,10,ubiquity-kdeui.desktop,770,10");
     desktop.writeConfig("sortDirsFirst", "false");
 
 }
