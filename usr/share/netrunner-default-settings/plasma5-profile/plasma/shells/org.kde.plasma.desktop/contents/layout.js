7ad9b0d833194e38e49111c6ee0625795c61c3fb
Leszek Lesner <leszek.lsner@web.de>
2015-05-11 11:03:29 +0200

    Show run , hide run associated application in right click contextmenu

--- usr/share/netrunner-default-settings/plasma5-profile/plasma/shells/org.kde.plasma.desktop/contents/layout.js
+++ usr/share/netrunner-default-settings/plasma5-profile/plasma/shells/org.kde.plasma.desktop/contents/layout.js
@@ -23,7 +23,7 @@ rightButton.writeEntry("_add panel", "true");
 rightButton.writeEntry("_context", "true");
 rightButton.writeEntry("_lock_screen", "false");
 rightButton.writeEntry("_logout", "false");
-rightButton.writeEntry("_run_command", "false");
+rightButton.writeEntry("_run_command", "true");
 rightButton.writeEntry("_sep1", "false");
 rightButton.writeEntry("_sep2", "true");
 rightButton.writeEntry("_sep3", "false");
@@ -35,6 +35,7 @@ rightButton.writeEntry("configure shortcuts", "false");
 rightButton.writeEntry("lock widgets", "true");
 rightButton.writeEntry("manage activities", "false");
 rightButton.writeEntry("remove", "true");
+rightButton.writeEntry("run associated application", "false");
 
 delete rightButton;
 delete actionPluginsDesktop;
