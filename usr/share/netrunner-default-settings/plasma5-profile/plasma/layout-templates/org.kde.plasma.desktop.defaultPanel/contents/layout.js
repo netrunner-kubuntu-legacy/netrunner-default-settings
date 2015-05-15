69966565ffbea2ed268bb694b3e50520318a65b5
Leszek Lesner <leszek.lsner@web.de>
2015-05-15 09:58:04 +0200

    Hide telepathy in systray

--- usr/share/netrunner-default-settings/plasma5-profile/plasma/layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
+++ usr/share/netrunner-default-settings/plasma5-profile/plasma/layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
@@ -54,7 +54,7 @@ var pvolumemixer = panel.addWidget("org.kde.plasma.volume")
 var systray = panel.addWidget("org.kde.plasma.systemtray")
 systray.currentConfigGroup = ["General"]
 systray.writeConfig("extraItems","org.kde.plasma.devicenotifier,org.kde.plasma.battery,org.kde.plasma.networkmanagement,org.kde.plasma.clipboard,org.kde.plasma.printmanager,org.kde.ktp-contactlist,org.kde.plasma.ktplegacypresenceapplet")
-systray.writeConfig("hiddenItems","org.kde.plasma.clipboard,KMix")
+systray.writeConfig("hiddenItems","org.kde.plasma.clipboard,KMix,org.kde.ktp-contactlist,org.kde.plasma.ktplegacypresenceapplet")
 
 panel.addWidget("org.kde.plasma.digitalclock")
 panel.addWidget("org.kde.plasma.notifications")
