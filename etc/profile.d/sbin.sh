ede1dcfd1460c4ff3602fb1877aadb6e8e8250c5
Rohan Garg <rohan@garg.io>
2015-08-19 15:59:17 +0200

    Add a script to export sbin to the PATH

---
+++ etc/profile.d/sbin.sh
@@ -0,0 +1,8 @@
+# Author : Rohan Garg <rohan@garg.io>
+# Export sbin path's on systems such as Debian that don't have
+# sbin exported to PATH
+
+if [[ $PATH != *"sbin"* ]]
+then
+    export PATH=$PATH:/usr/local/sbin:/sbin
+fi
